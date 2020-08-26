f7d3d65430b8348aa6cd53b32af9581773bcf56b
GitHub Action <action@github.com>
2020-08-26 12:57:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/tomaskulhanek/bodylighteditor/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tomaskulhanek/bodylighteditor/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tomaskulhanek.bodylighteditor.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tomaskulhanek.bodylighteditor.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/tomaskulhanek/bodylighteditor/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tomaskulhanek.bodylighteditor.services
+
+import com.github.tomaskulhanek.bodylighteditor.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
