9aec6d9497a5af00d0f06640ae66e965ea1347f0
Tomas Kulhanek <tomaton@centrum.cz>
2020-09-10 18:22:41 +0100

    UPDATE editor with toolbars, help interactive based on component icon, coverage of almost all bodylight components

--- src/components/editor.js
+++ src/components/editor.js
@@ -7,21 +7,26 @@ import jQuery from 'jquery';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {ContentUpdated} from './messages';
 import {inject} from 'aurelia-framework';
+import {BodylightEditorItems} from "./bodylightEditorItems";
 
 @inject(EventAggregator)
-export class Editor {
+export class Editor extends BodylightEditorItems{
+  indexhelpsrc = "index.md";
   constructor(ea) {
+    super();
     this.ea = ea;
+    this.helpsrc= this.indexhelpsrc;
   }
   attached() {
+
     window.$ = window.jQuery = jQuery;
     ace.require('ace/ext/language_tools');
     this.editor = ace.edit('editorref', {
       mode: 'ace/mode/markdown',
       theme: 'ace/theme/chrome',
-      enableBasicAutocompletion: true,
-      enableSnippets: false,
-      enableLiveAutocompletion: true,
+      enableBasicAutocompletion: false,
+      enableSnippets: true,
+      enableLiveAutocompletion: false,
       wrap: true,
       maxLines: 30,
       minLines: 30,
@@ -52,5 +57,77 @@ export class Editor {
         that.ea.publish(new ContentUpdated(content));
       }*/
     })
+  }
+
+  addItem(item){
+    this.editor.insert(item.def);
+    //document.getElementById('editorref').focus();
+    this.editor.focus();
+  }
+
+
+  showHelp(item){
+    //console.log("changing help",item);
+    if (item)
+      this.helpsrc = item.doc;
+    else
+      this.helpsrc = this.indexhelpsrc;
+  }
+
+  undo(){
+    this.editor.undo();
+  }
+
+  redo(){
+    this.editor.redo();
+  }
+
+  bold(){
+    this.editor.insert('****');
+    let p = this.editor.getCursorPosition();
+    this.editor.moveCursorTo(p.row,p.column-2);
+    this.editor.focus();
+  }
+
+  italic(){
+    this.editor.insert('__');
+    let p = this.editor.getCursorPosition();
+    this.editor.moveCursorTo(p.row,p.column-1);
+    this.editor.focus();
+  }
+
+  code(){
+    this.editor.insert('``');
+    let p = this.editor.getCursorPosition();
+    this.editor.moveCursorTo(p.row,p.column-1);
+    this.editor.focus();
+  }
+  equation(){
+    this.editor.insert('$$');
+    let p = this.editor.getCursorPosition();
+    this.editor.moveCursorTo(p.row,p.column-1);
+    this.editor.focus();
+  }
+
+
+  h1(){
+    this.editor.insert('# ')
+    this.editor.focus();
+
+  }
+  h2(){
+    this.editor.insert('## ')
+    this.editor.focus();
+
+  }
+  h3(){
+    this.editor.insert('### ')
+    this.editor.focus();
+
+  }
+  h4(){
+    this.editor.insert('#### ')
+    this.editor.focus();
+
   }
 }
---
+++ static/index.md
@@ -0,0 +1,9 @@
+## Interactive Bodylight Editor
+Edit text in middle pane (Editor).
+Use toolbar to add bodylight components or standard tags.
+* <button class="w3-padding-4 w3-border w3-round w3-theme-l4" style="margin-right:4px">component-name</button> inserts component into the cursor position of editor
+* <i class="fa fa-question-circle"></i> shows help for particular component
+Other toolbar adds standard editing tags:
+* <i class="fa fa-undo"></i> undo the operation of addin the item
+* <i class="fa fa-repeat"></i> redo the operation of addin the item
+* <i class="fa fa-bold fa-italic"></i>H1 H2 ... - adds bold,italic, header 1, header 2, ...
