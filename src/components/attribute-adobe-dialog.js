8de6cbce3929caaafb01647ac97089809c3c3685
Tomas Kulhanek <tomaton@centrum.cz>
2020-10-29 09:55:45 +0000

    ADD adobe name change based on src

--- src/components/attribute-adobe-dialog.js
+++ src/components/attribute-adobe-dialog.js
@@ -1,5 +1,33 @@
 import {AttributeDialog} from './attribute-dialog';
+import {Editorapi} from './editorapi';
+import {inject} from 'aurelia-framework';
+import {observable} from 'aurelia-framework';
 
+@inject(Editorapi)
 export class AttributeAdobeDialog extends AttributeDialog {
+  @observable adobesrc;
 
+  constructor(api) {
+    super(api);
+  }
+
+  attached() {
+    //console.log('AttributeAdobeDialog attached()');
+    this.adobesrc = this.api.askAttributesItemsArray[0].value;
+    //console.log('adobesrc', this.adobesrc);
+  }
+
+  activate(model) {
+    //console.log('AttributeAdobeDialog activate()');
+    this.adobesrc = this.api.askAttributesItemsArray[0].value;
+    //console.log('adobesrc',this.adobesrc)
+  }
+
+  adobesrcChanged(newvalue) {
+    //console.log('adobesrc changed:', newvalue);
+    this.api.askAttributesItemsArray[0].value = newvalue;
+    //objectname = file name without extension
+    let objectname = newvalue.split('.').slice(0, -1).join('.');
+    this.api.askAttributesItemsArray[3].value = objectname;
+  }
 }
