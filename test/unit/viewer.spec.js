80bbfcfe502ae7b9867eccb2c3be73c01ba93438
Tomas Kulhanek <tomaton@centrum.cz>
2020-11-02 14:51:40 +0000

    UPDATE unit test

--- test/unit/viewer.spec.js
+++ test/unit/viewer.spec.js
@@ -2,22 +2,22 @@ import {bootstrap} from 'aurelia-bootstrapper';
 import {StageComponent} from 'aurelia-testing';
 import {PLATFORM} from 'aurelia-pal';
 
-describe('Stage Editorapp Component', () => {
+describe('Stage Viewer Component', () => {
   let component;
 
   beforeEach(() => {
     component = StageComponent
-      .withResources(PLATFORM.moduleName('editorapp'))
-      .inView('<editorapp></editorapp>');
+      .withResources(PLATFORM.moduleName('../../src/components/viewer'))
+      .inView('<viewer></viewer>');
   });
 
   afterEach(() => component.dispose());
 
-  it('should editor and viewer divs', done => {
+  it('should create editor and viewer divs', done => {
     component.create(bootstrap).then(() => {
       const view = component.element;
       const divs = view.getElementsByTagName('div');
-      expect(divs.length).toBeGreaterThan(1);
+      expect(divs.length).toBeGreaterThanOrEqual(1);
       done();
     }).catch(e => {
       fail(e);
