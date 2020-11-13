a55a9b47acfb12efc7e208ccaa06700d6ddc4849
Tomas Kulhanek <tomaton@centrum.cz>
2020-11-13 15:29:05 +0000

    UPDATE demo content

--- src/components/project-files/bodylight-struct.js
+++ src/components/project-files/bodylight-struct.js
@@ -31,17 +31,36 @@ export const DEMOFILES = [
   {name: 'heart.gif', type: FTYPE.ANIMATEDGIF, active: false}
 ];
 
-export const DEMOCONTENT = '# Introduction \n' +
-  'you may use markdown to tag **bold** or __italic__ \n' +
+export const DEMOCONTENT = '# Introduction\n' +
+  '\n' +
+  'Markdown with Bodylight Components v.2\n' +
+  '\n' +
+  'You may use markdown to tag `**bold** or __italic__ ` so it will be rendered as **bold** or __italic__ \n' +
+  '\n' +
   '## Sections\n' +
   'sections can be added using one or multipla hash symbols `#`. \n' +
   '## Mathematics\n' +
   'katex is supported thus writing LATEX like syntax between `$` symbol will render the math.\n' +
-  'E.g. Dirichlet integral  $\\int_{0}^\\infty\\frac{\\sin(x)}{x}dx = \\frac{\\pi}{2}$\n' +
+  '\n' +
+  'E.g. Dirichlet integral `$\\int_{0}^\\infty\\frac{\\sin(x)}{x}dx = \\frac{\\pi}{2}$` is rendered:\n' +
+  '\n' +
+  '$\\int_{0}^\\infty\\frac{\\sin(x)}{x}dx = \\frac{\\pi}{2}$\n' +
+  '\n' +
   '## Bodylight components\n' +
-  'Bodylight web components can be rendered directly, use full xml-tags with closing tags not self-closing.\n' +
+  '\n' +
+  'Bodylight web components can be rendered directly, use full xml-tags with closing tags (not self-closing).\n' +
   '<bdl-range min="10" max="20" step="2" default="15"></bdl-range>\n' +
+  '\n' +
   '## Animation\n' +
-  'Adobe Animate can be exported into CreateJS components, these can be imported.\n' +
+  '\n' +
+  'Adobe Animate can be exported into CreateJS components, these can be imported and used with `<bdl-animate-adobe></bdl-animate-adobe>` component.\n' +
+  '\n' +
+  '## FMI \n' +
+  '\n' +
+  'Modelica models exported to FMU and compiled by Bodylight FMU Compiler can be imported and used with `<bdl-fmi></bdl-fmi>` component.\n' +
+  '\n' +
+  '## Charts\n' +
+  'Plotly, Chartjs and Dygraph libraries are supported use `<bdl-chartjs-time></bdl-chartjs-time>` or `<bdl-plotly></bdl-plotly>` or `<bdl-dygraph></bdl-dygraph>` components.\n' +
+  '\n' +
   '## Animated GIF\n' +
-  'Animated GIF can be imported, component `bdl-animate-gif` can handle animation and controls animation per each frame.';
+  'Animated GIF can be imported, component `bdl-animate-gif` can handle animation and controls animation per each frame.\n';
