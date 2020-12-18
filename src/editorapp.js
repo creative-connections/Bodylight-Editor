14aa2a138e17c547930aef95aab5bd1a55a03b01
Tomas Kulhanek <tomaton@centrum.cz>
2020-12-18 15:05:26 +0000

    UPDATE version

--- src/editorapp.js
+++ src/editorapp.js
@@ -1,11 +1,11 @@
-import {inject} from "aurelia-framework";
-import {Editorapi} from "./components/editorapi";
+import {inject} from 'aurelia-framework';
+import {Editorapi} from './components/editorapi';
+import {version} from '../package.json';
 
 @inject(Editorapi)
 export class Editorapp {
-
-  constructor(api){
+  constructor(api) {
     this.api = api;
+    this.version = version;
   }
-
 }
