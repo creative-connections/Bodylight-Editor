51752ff4302aced827e59e2f2916fb48ff975c12
TomasKulhanek <tmkulhanek@gmail.com>
2021-08-06 16:09:18 +0200

    UPDATE sidebar layout

--- src/components/animationbinding.js
+++ src/components/animationbinding.js
@@ -1,3 +1,11 @@
 export class Animationbinding {
+  bind() {
+    this.mapping = [{animobj: 'sipka', amin: 0, amax: 99, fmuvarname: 'x', fmin: 0, fmax: 1}];
+  }
+  identify(){
 
+  }
+  submit(){
+
+  }
 }
