fcf7a032e9380ecf5543c0fe7d748831e71ef69f
Tomas Kulhanek <tomaton@centrum.cz>
2021-11-29 16:39:54 +0100

    UPDATE resizable dialog column

--- src/editorapp.js
+++ src/editorapp.js
@@ -4,8 +4,37 @@ import {version} from '../package.json';
 
 @inject(Editorapi)
 export class Editorapp {
+  dialogwidth=0;
+  editorwidth=0;
+  moving=false;
   constructor(api) {
     this.api = api;
     this.version = version;
+  }
+
+  attached(){}
+
+  mouseDown(event) {
+    this.moving = true;
+    this.pageX = event.pageX;
+    this.dialogwidth = this.dialogdiv.offsetWidth;
+    this.editorwidth = this.editordiv.offsetWidth;
+    //console.log('editorapp mousedown pagex:'+this.pageX+' dialogwidth:'+this.dialogwidth+' editorwidth:'+this.editorwidth);
+    //console.log('dialogdiv',this.dialogdiv);
+    //console.log('dialogdiv',this.editordiv);
+  }
+
+  mouseMove(event) {
+    if (this.moving) {
+      let diffX = event.pageX - this.pageX;
+      this.dialogdiv.style.width = (this.dialogwidth + diffX) + 'px';
+      this.editordiv.style.width = (this.editorwidth - (diffX)-20) + 'px';
+      //console.log('editorapp mousemove diffX:'+diffX+' dialogwidth:'+this.dialogdiv.style.width+' editorwidth:'+this.editordiv.style.width);
+    }
+  }
+
+  mouseUp(event){
+    //console.log('editorapp mouseUp')
+    this.moving = false;
   }
 }
